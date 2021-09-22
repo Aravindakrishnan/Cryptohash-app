e91000e765b244d0e88b6f7ee9834578d353c547
Aravindakrishnan <aravindapks@gmail.com>
2021-09-23 01:00:40 +0530

    Modified Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <h4 className="navbar__title">HashCrypto 🪙 (#Top 100)</h4>
+            <h4 className="navbar__title">CryptoHashApp 🪙 (#Top 100)</h4>
            <a  href="https://github.com/Aravindakrishnan"><ion-icon name="logo-github"></ion-icon></a>
         </nav>
     )
